be4426999cc31e87d935be69f53e8ae2bf53fbf5
Paulo Victor Souza <p.victordsouza@gmail.com>
2023-10-11 07:42:01 -0300

    fix MuseumCreationDto

--- src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -3,6 +3,13 @@ package com.betrybe.museumfinder.dto;
 import com.betrybe.museumfinder.model.Coordinate;
 
 /** DTO to Museum. */
-public record MuseumDto(Long id, String name, String description, String address,
-                        String collectionType, String subject, String url, Coordinate coordinate) {
-}
+public record MuseumDto(
+    Long id,
+    String name,
+    String description,
+    String address,
+    String collectionType,
+    String subject,
+    String url,
+    Coordinate coordinate
+) { }
