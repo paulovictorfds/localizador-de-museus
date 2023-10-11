845954c184673bceee8d6e6ab61f839344174f76
Paulo Victor Souza <p.victordsouza@gmail.com>
2023-10-10 23:56:46 -0300

    creating classes and dto for museum

---
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -0,0 +1,9 @@
+package com.betrybe.museumfinder.dto;
+
+import com.betrybe.museumfinder.model.Coordinate;
+
+/** DTO to Museum Creation. */
+public record MuseumCreationDto(Long id, String name, String description, String address,
+                                String collectionType, String subject, String url,
+                                Coordinate coordinate) {
+}
